201b97228513116b6f29d98599c9e2b264e724a2
maxi0806 <maximourrutia806@gmail.com>
2025-07-07 12:33:23 -0300

    Actualizo galería y rutas de imágenes

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -3,14 +3,15 @@ import '../styles/galeria.css'
 import { Link } from 'react-router-dom'
 
 const imagenes = [
-  'pexels-eyad-tariq-2217318-3879495.jpg',
-  'imagen-cafeteria.jpg',
-  'pexels-ezequiel-campos-925408-29879921.jpg',
-  'pexels-kevinmenajang-982612.jpg',
-  'pexels-maxim-chizhevsky-484318817-15920111.jpg',
-  'pexels-nadin-sh-78971847-11214712.jpg',
-  'pexels-nataliaolivera-30632228.jpg'
+  '/galeria/pexels-eyad-tariq-2217318-3879495.jpg',
+  '/galeria/imagen-cafeteria.jpg',
+  '/galeria/pexels-ezequiel-campos-925408-29879921.jpg',
+  '/galeria/pexels-kevinmenajang-982612.jpg',
+  '/galeria/pexels-maxim-chizhevsky-484318817-15920111.jpg',
+  '/galeria/pexels-nadin-sh-78971847-11214712.jpg',
+  '/galeria/pexels-nataliaolivera-30632228.jpg'
 ]
+
 
 const Galeria = () => {
   return (
