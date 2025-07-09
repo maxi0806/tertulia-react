afbff01bd75730a8d49722a79f41a40132a0adea
maxi0806 <maximourrutia806@gmail.com>
2025-07-09 15:22:35 -0300

    actualice el menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,28 +8,28 @@ function Menu() {
           <img src="/img/cafe-con-medialunas.jpg" alt="Café y medialunas" />
           <h2>Desayuno Clásico</h2>
           <p>Incluye café con leche y medialunas recién horneadas.</p>
-          <button>Ver más</button>
+          <p className="fondo-p">4.500$</p>
         </div>
 
         <div className="card">
           <img src="/img/jugo-con-tostado.jpg" alt="Jugo y tostado" />
           <h2>Combo Saludable</h2>
           <p>Jugo natural con tostado de pan integral y queso fresco.</p>
-          <button>Ver más</button>
+          <p className="fondo-p">5.000$</p>
         </div>
 
         <div className="card">
           <img src="/img/yogur-frutas-del-bosque.jpg" alt="Yogur con frutas" />
           <h2>Desayuno Dulce</h2>
           <p>Yogur con frutas de estación, granola casera y un toque de miel natural.</p>
-          <button>Ver más</button>
+          <p className="fondo-p">6.000$</p>
         </div>
 
         <div className="card">
           <img src="/img/dasdaasdasda.jpg" alt="Sándwich clásico" />
           <h2>Sándwich Clásico</h2>
           <p>Pan artesanal con jamón cocido, queso, acompañado con bebida.</p>
-          <button>Ver más</button>
+          <p className="fondo-p">8.000$</p>
         </div>
 
       </main>
